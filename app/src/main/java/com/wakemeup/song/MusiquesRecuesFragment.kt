56e1ec283c5c5b7dee276f2a66c63736ef9df630
Rahard <quentin.rahard@gmail.com>
2020-05-05 14:57:20 +0200

    Pouvoir switch de liste ATTENTE / PASSEES
    Ajout de fonctions en lien avec les liveData dans le repo pour les sonneries recues

--- app/src/main/java/com/wakemeup/song/MusiquesRecuesFragment.kt
+++ app/src/main/java/com/wakemeup/song/MusiquesRecuesFragment.kt
@@ -5,110 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
-import com.wakemeup.AppWakeUp
-import com.wakemeup.MainActivity
 import com.wakemeup.R
-import com.wakemeup.util.loadFavoris
-import com.wakemeup.util.persisteFavoris
-import com.wakemeup.util.resetFavoris
 import kotlinx.android.synthetic.main.fragment_musiques_recues.view.*
-import kotlinx.android.synthetic.main.fragment_musiques_recues.view.texte_pas_de_musiques_en_attente
-import kotlinx.android.synthetic.main.fragment_video.view.pb_main_loader
 
 class MusiquesRecuesFragment : Fragment() {
 
-    private lateinit var dialogue : DialogueYoutube
-    private var isPlaying: Boolean = false
-    private val songList = mutableListOf<Song>()
-    private var favorisListe : MutableList<Song> = mutableListOf<Song>()
-
-    private lateinit var mAdapter: SongAdapter
-    private lateinit var youTubePlayerView: YouTubePlayerView
     private lateinit var currentView: View
 
-    private var currentIndex: Int = 0
-    private var currentSongLength: Int = 0
-    private var firstLaunch = true
-    private var currentSong: Song? = null
-
-    private var youTubePlayer: YouTubePlayer? = null
+    private var fragmentMusiquesPassees: MusiquesPasseesFragment? = null
+    private var fragmentMusiquesAttente: MusiquesAttenteFragment? = null
 
 
-
-
-    private fun changeSelectedSong(index: Int) {
-        mAdapter.notifyItemChanged(mAdapter.selectedPosition)
-        currentIndex = index
-        mAdapter.selectedPosition = currentIndex
-        mAdapter.notifyItemChanged(currentIndex)
-    }
-
-    //lance la video reliée au parametre "song"
-    private fun prepareSong(song: Song?) {
-        //todo vérifier bug ici, si on sélectionne la musique trop vite
-        if (song != null) {
-
-            currentSongLength = song.duration
-            currentSong = song
-
-            if (youTubePlayer != null) {
-                youTubePlayer!!.loadVideo(song.id, 0F)
-                //////// todo youTubePlayer.setVolume(100)
-                youTubePlayerView.getPlayerUiController()
-                    .setVideoTitle(song.title)//.loadVideo(videoId, 0F)
-
-                this.isPlaying = true
-            }
-        }
-    }
-
-    //TODO créer classe abstraite ou créer classe BouttonPartage
-
-
-    //Gestion du clic sur le bouton suprimer
-    private fun gestionBoutonSupprimer(){
-        val btSupprimer = currentView.bouton_supprimer_musiques_en_attente
-        btSupprimer.setOnClickListener {
-
-            if (currentSong != null) {
-                songList.remove(currentSong!!)
-                if (songList.isNullOrEmpty()){
-                    currentSong = null
-                    currentView.texte_pas_de_musiques_en_attente.visibility = View.VISIBLE
-                }
-
-                //Mise à jour-----------------
-
-                mAdapter.notifyDataSetChanged()
-                mAdapter.selectedPosition = 0
-                //------------------------------
-
-                resetFavoris(this.requireContext())
-                persisteFavoris(this.requireContext(),songList)
-
-                Toast.makeText(
-                    activity!!.application,
-                    "Vidéo Suprimmée",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-            }
-            else{
-                Toast.makeText(
-                    activity!!.application,
-                    "Veuillez sélectionner une vidéo",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-        }
-    }
 
     //Gestion du clic sur le bouton En attente ou Passees
     private fun gestionSwitchAttentePassees(){
@@ -117,63 +25,34 @@ class MusiquesRecuesFragment : Fragment() {
 
         btEnAttente.setOnClickListener {
             if (!btEnAttente.isActivated){
-                btEnAttente.setBackgroundColor(getResources().getColor(R.color.colorPrimary))
+                btEnAttente.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark))
                 btEnAttente.setTextColor(getResources().getColor(R.color.white))
 
                 btPassees.setBackgroundColor(getResources().getColor(R.color.grey))
-                btPassees.setTextColor(getResources().getColor(R.color.colorPrimary))
+                btPassees.setTextColor(getResources().getColor(R.color.colorPrimaryDark))
 
                 btPassees.isActivated = false
                 btEnAttente.isActivated = true
+
+                showFragment(FragmentId.FRAGMENT_ATTENTE)
             }
         }
 
         btPassees.setOnClickListener {
             if (!btPassees.isActivated){
-                btPassees.setBackgroundColor(getResources().getColor(R.color.colorPrimary))
+                btPassees.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark))
                 btPassees.setTextColor(getResources().getColor(R.color.white))
 
                 btEnAttente.setBackgroundColor(getResources().getColor(R.color.grey))
-                btEnAttente.setTextColor(getResources().getColor(R.color.colorPrimary))
+                btEnAttente.setTextColor(getResources().getColor(R.color.colorPrimaryDark))
 
                 btPassees.isActivated = true
                 btEnAttente.isActivated = false
+
+                showFragment(FragmentId.FRAGMENT_PASSEES)
             }
         }
     }
-
-    //Gestion du clic sur le bouton favori
-    private fun gestionBoutonFavori(){
-        val btFavori = currentView.bouton_ajouter_favori
-        btFavori.setOnClickListener {
-            if (AppWakeUp.auth.currentUser!!.isAnonymous) {
-                dialogue.createAlertDialogNotConnected(context!!, this.activity!! as MainActivity)
-            } else {
-                if (currentSong!=null) {
-                    gestionFavoris()
-                }
-                Toast.makeText(
-                    activity!!.application,
-                    "La video a été ajouté",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    private fun gestionFavoris(){
-        //Gestion de la persistance des favoris (sauvgarde les favoris dans un fichier)
-        if (loadFavoris(this.requireContext()) != null) {
-            favorisListe = loadFavoris(this.requireContext())!!
-        }
-        else{
-            favorisListe = mutableListOf<Song>()
-        }
-        favorisListe.add(currentSong!!)
-        resetFavoris(this.requireContext())
-        persisteFavoris(this.requireContext(),favorisListe)
-    }
-
 
 
     override fun onCreateView(
@@ -181,91 +60,86 @@ class MusiquesRecuesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        super.onCreate(savedInstanceState)
         currentView = inflater.inflate(R.layout.fragment_musiques_recues, container, false)
-
-        val recyclerView = currentView.recycler_list_video_musiques_en_attente
-        recyclerView.layoutManager = LinearLayoutManager(activity)
-        recyclerView.adapter = mAdapter
-
-
-        youTubePlayerView = currentView.youtube_player_view_musiques_recues
-        lifecycle.addObserver(youTubePlayerView)
-
-        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-            override fun onReady(ytPlayer: YouTubePlayer) {
-                youTubePlayer = ytPlayer
-                if (currentSong != null) {
-                    prepareSong(currentSong)
-                }
-            }
-        })
-
-        currentIndex = 0
-        currentView.pb_main_loader.visibility = View.GONE
-        songList.clear()
-
-        gestionBoutonSupprimer()
-        gestionBoutonFavori()
-        gestionSwitchAttentePassees()
-
-//        textePasDeFavori = currentView.findViewById(R.id.texte_pas_de_favori)
-
-        //Charge les musiques en attente -----------------------------------------------------
-        val musiques : MutableList<Song>? = null //todo AppWakeUp.listSonneriesEnAttente
-        if (musiques != null) {
-            songList.addAll(musiques)
-            if (musiques.isEmpty()) {
-                currentView.texte_pas_de_musiques_en_attente.visibility = View.VISIBLE
-            }
-            else{
-                currentView.texte_pas_de_musiques_en_attente.visibility=View.INVISIBLE
-            }
-
-            //Lancer la 1ere video youtube de la liste
-            if (songList.isNotEmpty()) {
-                currentSong = songList[0]
-                changeSelectedSong(0)
-                prepareSong(currentSong)
-            }
-            //----------------------------------------
-
-        }
-        else{
-            currentSong = null
-            currentView.texte_pas_de_musiques_en_attente.visibility = View.VISIBLE
-        }
-        //------------------------------------------------------------------------
-
-
-
-
-
-        mAdapter.notifyDataSetChanged()
-        mAdapter.selectedPosition = 0
-
-        dialogue = DialogueYoutube(activity!!)
-
         return currentView
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        gestionSwitchAttentePassees()
+        showFirstFragment()
+    }
+
+
+    // ---------------------
+    // FRAGMENTS
+    // ---------------------
+
+    // Show first fragment when activity is created
+    private fun showFirstFragment() {
+        val visibleFragment =
+            childFragmentManager.findFragmentById(R.id.child_fragment_container)
+        if (visibleFragment == null) {
+            // Show News Fragment
+            this.showFragment(FragmentId.FRAGMENT_PASSEES)
+        }
+    }
+
+    // Show fragment according an Identifier
+
+    private fun showFragment(fragmentIdentifier: FragmentId) {
+        when (fragmentIdentifier) {
+            FragmentId.FRAGMENT_PASSEES -> this.showPasseesFragment()
+            FragmentId.FRAGMENT_ATTENTE -> this.showAttenteFragment()
+        }
+    }
+
+
+
+    // ---
+
+    // Create each fragment page and show it
+
+    private fun showAttenteFragment() {
+        if (this.fragmentMusiquesAttente == null) {
+            this.fragmentMusiquesAttente = MusiquesAttenteFragment.newInstance(this.requireContext())
+        }
+        childFragmentManager.beginTransaction().remove(this.fragmentMusiquesAttente!!).commit()
+        this.startTransactionFragment(this.fragmentMusiquesAttente!!)
+    }
+
+    private fun showPasseesFragment() {
+        if (this.fragmentMusiquesPassees == null) {
+            this.fragmentMusiquesPassees = MusiquesPasseesFragment.newInstance(this.requireContext())
+        }
+        childFragmentManager.beginTransaction().remove(this.fragmentMusiquesPassees!!).commit()
+        this.startTransactionFragment(this.fragmentMusiquesPassees!!)
+    }
+
+    // ---
+
+    // Generic method that will replace and show a fragment inside the MainActivity Frame Layout
+    private fun startTransactionFragment(fragment: Fragment) {
+        if (!fragment.isVisible) {
+            childFragmentManager.beginTransaction()
+                .replace(R.id.child_fragment_container, fragment).commit()
+        }
+    }
+
 
     companion object {
 
         fun newInstance(ctx: Context): MusiquesRecuesFragment {
 
             val nf = MusiquesRecuesFragment()
-            nf.mAdapter = SongAdapter(ctx, nf.songList,
-                object : SongAdapter.RecyclerItemClickListener {
-                    override fun onClickListener(song: Song, position: Int) {
-                        nf.firstLaunch = false
-                        nf.changeSelectedSong(position)
-                        nf.prepareSong(song)
-                    }
-                })
+            // todo nf ????
             return nf
         }
-    }
 
+        enum class FragmentId {
+            FRAGMENT_PASSEES,
+            FRAGMENT_ATTENTE
+        }
+    }
 }
 
 
